3b09412e9a89610d2f0a6dc478a3cc154db423d1
Umar Aqeel <112412509+Umar2838@users.noreply.github.com>
2023-01-29 16:12:26 +0500

    added assignment of chapter 2

---
+++ app.js
@@ -0,0 +1,43 @@
+
+// Chapter 02 (Variable & String)
+
+// Question 01
+var username = "";
+// Question 02
+
+var myName = "Umar Aqeel";
+console.log(myName);
+
+// Question 03
+ var message="";
+ var message ="Hello World"
+alert(message);
+
+// Question 04
+var name ="Umar";
+alert (name);
+var age ="17 years old";
+alert(age);
+var cirtification = "Certified Web & Mobile Aplication";
+alert(cirtification);
+
+// Question 05
+var food = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(food);
+
+// Question 06
+myself = "My email Address is"
+var email ="umaraqeel@gmail.com";
+alert(myself+email);
+
+// Question 07
+trying="I am Trying to learn from the Book "
+var book ="A smarter\n way to learn JAVASCRIPT";
+alert(trying+book);
+
+// Question 08 
+document.write("YAH! I can write HTML content through JavaScript");
+
+// Question 09
+var design="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+alert(design);
